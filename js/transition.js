0af80ee1b4b14220a51c8c4f4301feb8d141cbdd
rikk626a <32450104+rikk626a@users.noreply.github.com>
2018-06-09 15:26:21 +0200

    animation + video + ux

---
+++ js/transition.js
@@ -0,0 +1,19 @@
+@keyframes fadeOut {
+     from {
+          opacity: 1;
+     }
+
+     to {
+          opacity: 0;
+     }
+}
+
+.fadeOut {
+     -webkit - animation - name: fadeOut;
+     animation - name: fadeOut;
+}
+
+@ - webkit - keyframes fadeOutDown {
+          from {
+               opacity: 1;
+          }
